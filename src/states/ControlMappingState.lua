55b23cd9acbb7e3572b010773f00d4b69fe4cbc4
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-05-11 17:20:31 +0530

    Add Support to change the player's control

--- src/states/ControlMappingState.lua
+++ src/states/ControlMappingState.lua
@@ -8,32 +8,38 @@ function ControlMappingState:init()
     -- The options in this state
     self.menu = Menu {
         {'Fullscreen', function()
-            -- Transition to the get control input state
-            gStateMachine:change('get-control-input', {
-                ball = self.ball,
-                player1 = self.player1,
-                player2 = self.player2,
-                -- The reference to this instance
-                controlMappingStateRef = self,
-                control = 'FULLSCREEN'
-            })
+            self:getControl('FULLSCREEN')
             return CONTROLS.FULLSCREEN
         end, CONTROLS.FULLSCREEN},
 
         {'Pause Menu', function()
-            -- Transition to the get control input state
-            gStateMachine:change('get-control-input', {
-                ball = self.ball,
-                player1 = self.player1,
-                player2 = self.player2,
-                -- The reference to this instance
-                controlMappingStateRef = self,
-                control = 'PAUSE'
-            })
+            self:getControl('PAUSE')
             return CONTROLS.PAUSE
         end, CONTROLS.PAUSE},
 
+        {'PLAYER 1 - UP', function()
+            self:getControl('PLAYER1_UP')
+            return CONTROLS.PLAYER1_UP
+        end, CONTROLS.PLAYER1_UP},
+
+        {'PLAYER 1 - DOWN', function()
+            self:getControl('PLAYER1_DOWN')
+            return CONTROLS.PLAYER1_DOWN
+        end, CONTROLS.PLAYER1_DOWN},
+
+        {'PLAYER 2 - UP', function()
+            self:getControl('PLAYER2_UP')
+            return CONTROLS.PLAYER2_UP
+        end, CONTROLS.PLAYER2_UP},
+
+        {'PLAYER 2 - DOWN', function()
+            self:getControl('PLAYER2_DOWN')
+            return CONTROLS.PLAYER2_DOWN
+        end, CONTROLS.PLAYER2_DOWN},
+
         {'Back to Settings', function()
+            -- Refersh the player controls
+            self:refreshPlayerControls()
             -- Transition back to the settings
             gStateMachine:change('settings', {
                 ball = self.ball,
@@ -75,9 +81,48 @@ function ControlMappingState:render()
     love.graphics.printf("Controls", 0, VIRTUAL_HEIGHT/2 - 3*gFontSize['large'], VIRTUAL_WIDTH, 'center')
 
     -- Render the menu
-    self.menu:render((VIRTUAL_HEIGHT/2)*0.85, 'medium')
+    self.menu:render((VIRTUAL_HEIGHT/2)*0.4, 'medium')
 
     -- Set the color to default
     love.graphics.setColor(COLORS.DEFAULT)
 
+end
+
+
+function ControlMappingState:getControl(controlName)
+    -- Transition to the get control input state
+    gStateMachine:change('get-control-input', {
+        ball = self.ball,
+        player1 = self.player1,
+        player2 = self.player2,
+        -- The reference to this instance
+        controlMappingStateRef = self,
+        control = controlName
+    })
+end
+
+function ControlMappingState:refreshPlayerControls()
+    --[[
+        Initializing the two players
+    ]]
+    PLAYER['1'].controls = {
+        up = CONTROLS.PLAYER1_UP,
+        down = CONTROLS.PLAYER1_DOWN
+    }
+    PLAYER['2'].controls = {
+        up = CONTROLS.PLAYER2_UP,
+        down = CONTROLS.PLAYER2_DOWN
+    }
+
+    self.player1 = Paddle {
+        name = 'Player 1',
+        player = PLAYER['1'],
+        score = 0
+    }
+
+    self.player2 = Paddle {
+        name = 'Player 2',
+        player = PLAYER['2'],
+        score = 0
+    }
 end
