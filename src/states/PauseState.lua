57dfd0ca2a9829592c854da5b9f73f5f5e0c70db
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-05-11 12:00:15 +0530

    Add menu to the Pause State and fix sound issues

--- src/states/PauseState.lua
+++ src/states/PauseState.lua
@@ -6,46 +6,37 @@ PauseState = Class{__includes = BaseState}
 function PauseState:init()
     if not GAME.SETTINGS.ZA_WARUDO then return end
 
-    self.wait_time = 0
+    -- To keep track of how long the game has been paused
     self.seconds_since_paused = 0
+
+    -- To disable the menu controls during any transition
+    self.canChooseOption = false
     
     -- Colors in the UI of paused state
     self.paused_opacity = 0
     self.neutral_opacity = 0
 
-    -- Interval of 1 second
-    self.interval = 1
+    -- The menu highlight color
+    self.highlighted = 1
 
-    -- Exit when this many seconds have passed
-    self.exit_seconds = 9
-
-    Timer.tween(2, {
-        -- tween bird's X to endX over bird.rate seconds
+    Timer.tween(1, {
+        -- Entering in the Pause state
         [self] = {
-            wait_time = 3,
             paused_opacity = 1,
             neutral_opacity = 1
         }
     }):finish(function()
-        -- Now show the timer
-        self.show_timer = true
-        self.wait_timer = 0
+        -- We can choose the option now
+        self.canChooseOption = true
 
-        Timer.tween(self.exit_seconds, {
-            [self] = {seconds_since_paused = self.exit_seconds}
-        }):finish(function()
-            -- Just making sure we're on the same page
-            paused_opacity = 1
-            neutral_opacity = 1
-
-            Timer.tween(2, {
-                [self] = {paused_opacity = 0, neutral_opacity = 0}
-            }):finish(function() self:unpause() end)
-
+        -- Start the pause menu timer
+        Timer.every(1, function()
+            self.seconds_since_paused = self.seconds_since_paused + 1
         end)
-        
+
+        -- Start the time-loop sound
         if GAME.SETTINGS.ZA_WARUDO then
-            gSounds:stop('za-warudo-20-sec-loop')
+            gSounds:get('za-warudo-20-sec-loop'):setLooping(true)
             gSounds:play('za-warudo-20-sec-loop')
         end
     end)
@@ -91,7 +82,7 @@ function PauseState:exit()
         gSounds:play('bg-music')
     end
 
-    -- Loss the access to playstate instance
+    -- Loose the access to playstate instance
     self.playstate_instance = nil
 end
 
@@ -99,19 +90,66 @@ function PauseState:update(dt)
     --[[
         If we press escape then exit
     ]]
-    if love.keyboard.wasPressed('escape') then
-        love.event.quit()
-    end
-
-    --[[
-        If we press 'p' then unpause the game
-    ]]
-    if love.keyboard.wasPressed('p') then
-        self:unpause()
-    end
 
     -- Updating the timer for wa-zarudo
     if GAME.SETTINGS.ZA_WARUDO then Timer.update(dt) end
+
+    -- --[[
+    --     If we press 'p' then unpause the game
+    -- ]]
+    -- if love.keyboard.wasPressed('p') then
+    --     self:unpause()
+    -- end
+
+    -- If the timer hasn't begun, then don't let the player move in the menu
+    if not self.canChooseOption then
+        return
+    end
+
+    -- If the user presses the ENTER or RETURN key, then transition accordinagly
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+
+        -- So that we can't change the option midway our transition
+        self.canChooseOption = false
+
+        -- Stop the time-loop sound
+        gSounds:stop('za-warudo-20-sec-loop')
+        -- Play the exit sound
+        gSounds:play('za-warudo-20-sec-exit')
+
+        if self.highlighted == 1 then
+            -- play the confirm sound
+            gSounds:stop('confirm')
+            gSounds:play('confirm')
+            self:unpause()
+
+        elseif self.highlighted == 2 then
+            -- play the confirm sound
+            gSounds:stop('confirm')
+            gSounds:play('confirm')
+            -- Back to the main menu
+            gStateMachine:change('title', {
+                ball = self.ball,
+                player1 = self.player1,
+                player2 = self.player2
+            })
+        end
+    end
+    
+    -- We will cycle the value of self.highlighted between those two
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('w') then
+        -- Play the select sound
+        gSounds:stop('select')
+        gSounds:play('select')
+        self.highlighted = (self.highlighted > 1) and self.highlighted - 1 or 2
+    end
+
+    if love.keyboard.wasPressed('down') or love.keyboard.wasPressed('s') then
+        -- Play the select sound
+        gSounds:stop('select')
+        gSounds:play('select')
+        self.highlighted = (self.highlighted < 2) and self.highlighted + 1 or 1
+    end
 end
 
 function PauseState:render()
@@ -119,8 +157,11 @@ function PauseState:render()
     self.playstate_instance:render(COLORS.PAUSED_SCREEN_COLOR)
 
     -- Putting an overlay on top of it
+    local menuActiveColor = {COLORS.PAUSED_MENU_CHOOSE.ACTIVE[1], COLORS.PAUSED_MENU_CHOOSE.ACTIVE[2], COLORS.PAUSED_MENU_CHOOSE.ACTIVE[3], self.paused_opacity}
+    local menuInactiveColor = {COLORS.PAUSED_MENU_CHOOSE.INACTIVE[1], COLORS.PAUSED_MENU_CHOOSE.INACTIVE[2], COLORS.PAUSED_MENU_CHOOSE.INACTIVE[3], self.paused_opacity}
+
     --[[
-        This can be a pause menu in future
+        This is the pause menu
     ]]
     -- Displaying the seconds passed since we paused the game
     love.graphics.setFont(gFonts['large'])
@@ -133,6 +174,26 @@ function PauseState:render()
         love.graphics.printf(tostring(math.floor(self.seconds_since_paused)) .. " seconds has passed", 0, VIRTUAL_HEIGHT/2 + gFontSize['large']/2 + gFontSize['small']/2, VIRTUAL_WIDTH, 'center')
     end
 
+    --[[
+        The options
+    ]]
+    love.graphics.setFont(gFonts['medium'])
+    love.graphics.setColor(menuInactiveColor)
+
+    if self.highlighted == 1 then
+        love.graphics.setColor(menuActiveColor)
+    end
+    -- Printing option 1
+    love.graphics.printf("Resume", 0, VIRTUAL_HEIGHT/2 + 4*gFontSize['medium'], VIRTUAL_WIDTH, 'center')
+
+    -- Set the color to inactive
+    love.graphics.setColor(menuInactiveColor)
+    if self.highlighted == 2 then
+        love.graphics.setColor(menuActiveColor)
+    end
+    -- Printing option 2
+    love.graphics.printf("Main Menu", 0, VIRTUAL_HEIGHT/2 + 6*gFontSize['medium'], VIRTUAL_WIDTH, 'center')
+
     -- Revert to original colors
     love.graphics.setColor(COLORS.DEFAULT)
 end
@@ -141,17 +202,24 @@ end
     Utility functions
 ]]
 function PauseState:unpause()
-    -- Unpause and revert to play state
-    gStateMachine:change('play', {
-        ball = self.ball,
-        player1 = self.player1,
-        player2 = self.player2,
-        game_mode = self.game_mode,
+    -- We will make a sweet transition
+    paused_opacity = 1
+    neutral_opacity = 1
 
-        defensive_bonus = self.defensive_bonus,
-        corner_bonus = self.corner_bonus,
+    -- Transition the opacity
+    Timer.tween(2, {
+        [self] = {paused_opacity = 0, neutral_opacity = 0}
+    }):finish(function()
+        -- Unpause and revert to play state
+        gStateMachine:change('play', {
+            ball = self.ball,
+            player1 = self.player1,
+            player2 = self.player2,
+            game_mode = self.game_mode,
+    
+            defensive_bonus = self.defensive_bonus,
+            corner_bonus = self.corner_bonus,
+        })
+    end)
 
-        -- We're entering the game from paused state
-        from_paused = true
-    })
 end
