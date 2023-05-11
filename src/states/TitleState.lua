483a9432fa0dfe15d2fd4a0926dd08d768476110
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-05-11 13:09:01 +0530

    Shift from manual Menu implementation to class Menu implementation

--- src/states/TitleState.lua
+++ src/states/TitleState.lua
@@ -19,6 +19,38 @@ function TitleState:init()
             self.colors[i] = self.colors[i - 1]
         end
     end)
+
+    -- Initialize the menu
+    self.menu = Menu {
+        {'Start Game', function()
+            gStateMachine:change('serve', {
+                ball = self.ball,
+                player1 = self.player1,
+                player2 = self.player2
+            })
+        end},
+        {'Settings', function()
+            -- Transition to the settings state
+            gStateMachine:change('settings', {
+                ball = self.ball,
+                player1 = self.player1,
+                player2 = self.player2
+            })
+        end},
+        {'Tips & Credits', function()
+            -- Transition to the credits and tips
+            gStateMachine:change('tips-credits', {
+                ball = self.ball,
+                player1 = self.player1,
+                player2 = self.player2
+            })
+        end},
+        {'Exit Game', function()
+            -- Quit the game with ease
+            love.event.quit()
+        end}, 
+    }
+
 end
 
 function TitleState:enter(params)
@@ -37,70 +69,9 @@ function TitleState:enter(params)
 end
 
 function TitleState:update(dt)
-    -- For choosing between different options
-
-    -- If the user presses the ENTER or RETURN key, then transition accordinagly
-    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        if self.highlighted == 1 then
-            -- play the confirm sound
-            gSounds:stop('confirm')
-            gSounds:play('confirm')
-            -- Transition to the play state
-            -- gStateMachine:change('choose-game-mode', {
-            --     ball = self.ball,
-            --     player1 = self.player1,
-            --     player2 = self.player2
-            -- })
-            gStateMachine:change('serve', {
-                ball = self.ball,
-                player1 = self.player1,
-                player2 = self.player2
-            })
-            
-        elseif self.highlighted == 2 then
-            -- play the confirm sound
-            gSounds:stop('confirm')
-            gSounds:play('confirm')
-            -- Transition to the settings state
-            gStateMachine:change('settings', {
-                ball = self.ball,
-                player1 = self.player1,
-                player2 = self.player2
-            })
-
-        elseif self.highlighted == 3 then
-            -- play the confirm sound
-            gSounds:stop('confirm')
-            gSounds:play('confirm')
-            -- Transition to the credits and tips
-            gStateMachine:change('tips-credits', {
-                ball = self.ball,
-                player1 = self.player1,
-                player2 = self.player2
-            })
-        elseif self.highlighted == 4 then
-            -- play the confirm sound
-            gSounds:stop('confirm')
-            gSounds:play('confirm')
-            -- Quit the game with ease
-            love.event.quit()
-        end
-    end
+    -- The menu we've created
+    self.menu:update(dt)
     
-    -- We will cycle the value of self.highlighted between those two
-    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('w') then
-        -- Play the select sound
-        gSounds:stop('select')
-        gSounds:play('select')
-        self.highlighted = (self.highlighted > 1) and self.highlighted - 1 or 4
-    end
-
-    if love.keyboard.wasPressed('down') or love.keyboard.wasPressed('s') then
-        -- Play the select sound
-        gSounds:stop('select')
-        gSounds:play('select')
-        self.highlighted = (self.highlighted < 4) and self.highlighted + 1 or 1
-    end
     --[[
         Checking keyboard bindings
     ]]
@@ -118,46 +89,14 @@ function TitleState:render()
     --[[
         Rendering our title screen
     ]]
-    -- The game title
     love.graphics.setColor(self.colors[1])
+    -- The font of the Title
     love.graphics.setFont(gFonts['large'])
-
+    -- The game title
     love.graphics.printf(GAME.TITLE, 0, VIRTUAL_HEIGHT/2 - 3*gFontSize['large'], VIRTUAL_WIDTH, 'center')
-
-    -- Giving some options
-    love.graphics.setFont(gFonts['medium'])
-    love.graphics.setColor(COLORS.MENU_CHOOSE.INACTIVE)
-
-    if self.highlighted == 1 then
-        love.graphics.setColor(COLORS.MENU_CHOOSE.ACTIVE)
-    end
-    -- Printing option 1
-    love.graphics.printf('Start Game', 0, VIRTUAL_HEIGHT/2 - gFontSize['medium'], VIRTUAL_WIDTH, 'center')
-
-    -- Set the color to inactive
-    love.graphics.setColor(COLORS.MENU_CHOOSE.INACTIVE)
-    if self.highlighted == 2 then
-        love.graphics.setColor(COLORS.MENU_CHOOSE.ACTIVE)
-    end
-    -- Printing option 2
-    love.graphics.printf('Settings', 0, VIRTUAL_HEIGHT/2 + gFontSize['medium'], VIRTUAL_WIDTH, 'center')
-
-    -- Set the color to inactive
-    love.graphics.setColor(COLORS.MENU_CHOOSE.INACTIVE)
-    if self.highlighted == 3 then
-        love.graphics.setColor(COLORS.MENU_CHOOSE.ACTIVE)
-    end
-    -- Printing option 2
-    love.graphics.printf('Tips & Credits', 0, VIRTUAL_HEIGHT/2 + gFontSize['medium']*3, VIRTUAL_WIDTH, 'center')
-
-    -- Set the color to inactive
-    love.graphics.setColor(COLORS.MENU_CHOOSE.INACTIVE)
-    if self.highlighted == 4 then
-        love.graphics.setColor(COLORS.MENU_CHOOSE.ACTIVE)
-    end
-    -- Printing option 3
-    love.graphics.printf('Exit Game', 0, VIRTUAL_HEIGHT/2 + gFontSize['medium']*5, VIRTUAL_WIDTH, 'center')
-
+    
+    -- The menu we request
+    self.menu:render(VIRTUAL_HEIGHT/2, 'medium')
 
     -- Set the color to default
     love.graphics.setColor(COLORS.DEFAULT)
